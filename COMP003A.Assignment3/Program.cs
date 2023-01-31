d777fde1d5e8cce5c728103ab71ef228f9f89ee0
BenaJovi <xavibenavides@yahoo.com>
2023-01-30 22:17:43 -0800

    Add project files.

---
+++ COMP003A.Assignment3/Program.cs
@@ -0,0 +1,80 @@
+﻿//  Author: Jovani Benavides
+ // Course: COMP - 003A
+ // Purpose: 
+ // 
+
+
+namespace COMP003A.Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Please Enter Total Grade (0-100): ");
+            string grade = Console.ReadLine();
+            double TotalGrade = Convert.ToDouble(grade);
+
+            if (TotalGrade >= 90 && TotalGrade == 100)
+            {
+                Console.WriteLine("Letter Grade: A");
+                
+            }
+            else if (TotalGrade >= 80 && TotalGrade <= 89)
+            {
+                Console.WriteLine("Letter Grade: B");
+            }
+            else if (TotalGrade >= 70 && TotalGrade <= 79)
+            {
+                Console.WriteLine("Letter Grade: C");
+            }
+            else if (TotalGrade >= 60 && TotalGrade <= 69)
+            {
+                Console.WriteLine("Letter Grade: D");
+            }
+            else if (TotalGrade >=0 && TotalGrade <= 60)
+            {
+                Console.WriteLine("Letter Grade: F");
+            }
+            else
+            {
+              Console.WriteLine("Invalid Input!");
+            }
+           
+            Console.WriteLine("*****************************************");
+            Console.Write("Enter an Integer day of the week (1-7):");
+            string WeekNum = Console.ReadLine();
+            int numericDay = Convert.ToInt32(WeekNum);
+           
+            switch (numericDay) 
+            {
+                case 1:
+                    Console.WriteLine("String Day: Monday");
+                    break; 
+                case 2:
+                    Console.WriteLine("String Day: Tuesday");
+                    break;  
+                case 3:
+                    Console.WriteLine("String Day: Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("String Day: Thurday");
+                    break;
+                case 5:
+                    Console.WriteLine("String Day: Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("String Day: Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("String Day: Sunday");
+                    break;
+                default: 
+                    Console.WriteLine("Inavlid Day!");
+                    break;
+            }
+
+            
+        }
+    }
+}
